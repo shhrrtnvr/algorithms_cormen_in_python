cecb9cefcfb2d8d63e8ec4c054caa2606d6f0ec5
shhrrtnvr <shhrrtnvr@gmail.com>
2020-07-16 18:48:45 +0600

    merge sort updated

--- 2.3.1_merge_sort.py
+++ 2.3.1_merge_sort.py
@@ -1,6 +1,6 @@
 import math
 def merge(A, p, q, r):
-    L, R = A[p:q+1] + [math.inf], A[q+1:r] + [math.inf]
+    L, R = A[p:q] + [math.inf], A[q:r] + [math.inf]
     i, j = 0, 0
     while i+j < r-p:
         if L[i] <= R[j]:
@@ -10,15 +10,18 @@ def merge(A, p, q, r):
             A[p+i+j] = R[j]
             j += 1
 
-def merge_srt(A, p, r):
-    if p >= r: return
-    q = (p + r) // 2
-    merge_srt(A, p, q)
-    merge_srt(A, q+1, r)
+def merge_sort(A, *args):
+    if args:
+        p, r = args 
+    else:
+        merge_sort(A, 0, len(A))
+        return
+    if r-p <= 1: return 
+    q = (p + r + 1) // 2 
+    merge_sort(A, p, q)
+    merge_sort(A, q, r)
     merge(A, p, q, r)
 
-def merge_sort(A):
-    merge_srt(A, 0, len(A))
 
 if __name__ == "__main__":
     import random
